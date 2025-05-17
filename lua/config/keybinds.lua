bd2174b492e297c277dd53afe5990271ca719a8b
NDJafu <giaphu432@gmail.com>
2025-05-17 17:18:51 +0700

    update: keymap & netrw

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>bd", vim.cmd.bd)
+vim.keymap.set("n", "<leader>qq", vim.cmd.wqa)
+vim.keymap.set("n", "<leader>ws", vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>wc", vim.cmd.close)
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
